d346fcdec5fb3ada54b8fc587ff040bbcaf86f5b
RoscaAlexandru775 <roscaalexandru160@gmail.com>
2023-05-09 20:52:26 +0300

    Initialize project

---
+++ Civic Guard/Civic_GuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Civic_GuardApp.swift
+//  Civic Guard
+//
+//  Created by Alexandru Rosca on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Civic_GuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
